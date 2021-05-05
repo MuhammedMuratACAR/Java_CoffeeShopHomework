1b716968767b2f52f30fa83bdbb2dc25773162cc
Muhammed Murat ACAR <57721006+MuhammedMuratACAR@users.noreply.github.com>
2021-05-05 19:48:50 +0300

    Delete eclipse-workspace/Lesson3 directory

--- eclipse-workspace/Lesson3/src/Lesson3/StudentManager.java
+++
@@ -1,7 +0,0 @@
-package Lesson3;
-
-public class StudentManager {
-	public void addComment(User user,String mesaj) {
-		System.out.println(user.getFirstName() + " adlı kullanıcın yorumu : " +  mesaj);
-	}
-}
