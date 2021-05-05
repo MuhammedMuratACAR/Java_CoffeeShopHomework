2c8fd2a3a72282de9157f90c44616eb8ff4a06f7
Muhammed Murat ACAR <57721006+MuhammedMuratACAR@users.noreply.github.com>
2021-05-06 02:00:00 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ import Entities.Customer;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 		BaseCustomerManager customerManager=new StarbucksCustomerManager(new MernisServiveAdapter());
 		customerManager.save(new Customer(1,"Muhammed Murat","Acar",LocalDate.of(1996,8,2),"12312312312"));
 		
